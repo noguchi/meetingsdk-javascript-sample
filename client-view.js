f9065c2328b91549d4f5d2539883c99cc49be635
Shingo Noguchi <noguchi@daifukuya.com>
2023-10-14 01:03:32 +0900

    write app client id

--- client-view.js
+++ client-view.js
@@ -1,50 +1,52 @@
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.17.0/lib', '/av')
+ZoomMtg.setZoomJSLib("https://source.zoom.us/2.17.0/lib", "/av");
 
-ZoomMtg.preLoadWasm()
-ZoomMtg.prepareWebSDK()
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
 // loads language files, also passes any error messages to the ui
-ZoomMtg.i18n.load('en-US')
-ZoomMtg.i18n.reload('en-US')
+ZoomMtg.i18n.load("en-US");
+ZoomMtg.i18n.reload("en-US");
 
-var authEndpoint = ''
-var sdkKey = ''
-var meetingNumber = '123456789'
-var passWord = ''
-var role = 0
-var userName = 'JavaScript'
-var userEmail = ''
-var registrantToken = ''
-var zakToken = ''
-var leaveUrl = 'https://zoom.us'
+var authEndpoint = "http://localhost:4000";
+var sdkKey = "T3_aSq5mQhqAP5_UpjleGQ";
+var meetingNumber = "75454679404";
+var passWord = "bw1Uvk";
+var role = 0;
+var userName = "from js client";
+var userEmail = "";
+var registrantToken = "";
+var zakToken = "";
+var leaveUrl = "https://zoom.us";
 
 function getSignature() {
   fetch(authEndpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       meetingNumber: meetingNumber,
-      role: role
-    })
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    startMeeting(data.signature)
-  }).catch((error) => {
-  	console.log(error)
+      role: role,
+    }),
   })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      startMeeting(data.signature);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function startMeeting(signature) {
-
-  document.getElementById('zmmtg-root').style.display = 'block'
+  document.getElementById("zmmtg-root").style.display = "block";
 
   ZoomMtg.init({
     leaveUrl: leaveUrl,
     success: (success) => {
-      console.log(success)
+      console.log(success);
       ZoomMtg.join({
         signature: signature,
         sdkKey: sdkKey,
@@ -55,15 +57,15 @@ function startMeeting(signature) {
         tk: registrantToken,
         zak: zakToken,
         success: (success) => {
-          console.log(success)
+          console.log(success);
         },
         error: (error) => {
-          console.log(error)
+          console.log(error);
         },
-      })
+      });
     },
     error: (error) => {
-      console.log(error)
-    }
-  })
+      console.log(error);
+    },
+  });
 }
